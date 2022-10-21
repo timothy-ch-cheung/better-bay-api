6f09a9c8af250b1dadb81d36abbf07d6023e66af
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2022-10-22 00:00:44 +0100

    Return cheapest items on GET

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-require('dotenv').config()
-const express = require('express');
-const EbayClient = require('ebay-oauth-nodejs-client');
 
-const ebayAuthToken = new EbayClient({
-    clientId: process.env.EBAY_CLIENT_ID,
-    clientSecret: process.env.EBAY_CLIENT_SECRET,
-    redirectUri: process.env.EBAY_REDIRECT_URI
-});
-
-(async () => {
-    const token = await ebayAuthToken.getApplicationToken('PRODUCTION');
-    console.log(token);
-})();
-
+import dotenv from "dotenv"
+import express from "express"
+import { buildBetterBayClient } from "better-bay-common"
+dotenv.config();
 const app = express();
-
 const port = 3000;
 
+const client = await buildBetterBayClient(
+    process.env.EBAY_CLIENT_ID,
+    process.env.EBAY_CLIENT_SECRET,
+    process.env.EBAY_REDIRECT_URI,
+    true
+);
+
 app.get('/v1/items/cheapest', (req, res) => {
-    res.send('WIP come back later!');
+    const itemIds = req.query.ids.split(',')
+
+    client.getCheapestItems(itemIds).then(items => {
+        res.send(items);
+    })
+
 });
 
 app.listen(port, () => console.log(`Better Bay API listening on port ${port}!`))
