e68c92fdedbe000c0800a1b43910c99adbf097e1
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2022-11-06 02:52:18 +0000

    Remove ignored files

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import express from "express";
-import { CheapestItemRequest } from "./types.js";
-export declare const client: import("better-bay-common").BetterBayClient;
-export declare function cheapestItemHandler(req: CheapestItemRequest, res: express.Response): void;
